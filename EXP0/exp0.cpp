0ea6fff25940f2577ba5b8b694612475fc207d14
Yuqing_Chen <chenyuqing0429@foxmail.com>
2021-04-02 17:33:09 +0800

    没搞定exp0_2

--- EXP0/exp0.cpp
+++ EXP0/exp0.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-    Image *img = new Image("qiuqiu.bmp");
+    BMP *img = new BMP("qiuqiu.bmp");
     img->to_txt("1.txt", 5, 5);
+
+    //Point l_b = {img->width / 3, img->height / 3};
+    Point l_b = {0, 0};
+    Point r_t = {img->width * 2 / 3, img->height * 2 / 3};
+    img->changeInRectangle(get_random_color(), l_b, r_t);
+    BMP::create_new_bmp(img->bmp_header, (Pixel **)img->pixels, img->width, img->height, "2.bmp");
 }
--- image_headers/DIP.hpp
+++ image_headers/DIP.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-#define HEADER_LENGTH 54
+#define BMP_HEADER_LENGTH 54
 #define COLOR_COPONENTS 3
 
 // 最大支持8K图片
@@ -20,23 +20,25 @@ typedef struct Point
     int y;
 } Point;
 
-class Image
+class BMP
 {
 private:
     FILE *fp;
-    char image_header[HEADER_LENGTH];
-    int32_t width;
-    int32_t height;
 
 public:
-    Image(const char *file_name);
-    ~Image();
+    BMP(const char *file_name);
+    ~BMP();
+    int32_t width;
+    int32_t height;
     Pixel pixels[MAX_ROW][MAX_COL];
+    char bmp_header[BMP_HEADER_LENGTH];
     void to_txt(const char *outfile_name, int row, int col);
-    void changeInRectangle(Pixel target_pixel, Point left_top, Point right_bottom);
+    void changeInRectangle(Pixel target_pixel, Point left_bottom, Point right_top);
+
+    static void create_new_bmp(char *header, Pixel **pixels, int width, int height, const char *target_file_name);
 };
 
-Image::Image(const char *file_name)
+BMP::BMP(const char *file_name)
 {
     // 获取图像长宽
     const int offset = sizeof(uint16_t) * 3 + sizeof(uint32_t) * 3;
@@ -54,12 +56,12 @@ Image::Image(const char *file_name)
     }
 
     fseek(fp, 0, SEEK_SET);
-    fread(image_header, 1, HEADER_LENGTH, fp);
+    fread(bmp_header, 1, BMP_HEADER_LENGTH, fp);
     fread(pixels, COLOR_COPONENTS, width * height, fp);
     fclose(fp);
 }
 
-void Image::to_txt(const char *outfile_name, int row, int col)
+void BMP::to_txt(const char *outfile_name, int row, int col)
 {
     if (row > width || col > height)
     {
@@ -78,11 +80,42 @@ void Image::to_txt(const char *outfile_name, int row, int col)
     fclose(fp);
 }
 
-void Image::changeInRectangle(Pixel target_pixel, Point left_top, Point right_bottom)
+void BMP::create_new_bmp(char *header, Pixel **bmp_pixels, int width, int height, const char *target_file_name)
 {
+    FILE *fout = fopen(target_file_name, "wb");
+    fwrite(header, BMP_HEADER_LENGTH, 1, fout);
+    fwrite(bmp_pixels, COLOR_COPONENTS, width * height, fout);
+    fclose(fout);
 }
 
-Image::~Image()
+void BMP::changeInRectangle(Pixel target_pixel, Point left_bottom, Point right_top)
+{
+    std::cout << (int)target_pixel.R << ' ' << (int)target_pixel.G << ' ' << (int)target_pixel.B << std::endl;
+    for (int i = left_bottom.x; i < right_top.x; i++)
+    {
+        for (int j = left_bottom.y; j < right_top.y; j++)
+        {
+            pixels[i][j] = target_pixel;
+        }
+    }
+}
+
+BMP::~BMP()
 {
     delete (fp);
+}
+
+Pixel get_random_color()
+{
+    srand((unsigned)time(NULL));
+    /* return [a, b] */
+    int a = 0;
+    int b = 255;
+
+    int r = (rand() % (b - a + 1)) + a;
+    int g = (rand() % (b - a + 1)) + a;
+    int _b = (rand() % (b - a + 1)) + a;
+    Pixel temp = {_b, g, r};
+
+    return temp;
 }
